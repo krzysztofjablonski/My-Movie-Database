657c47e37fd7a58564b5ed1f0408721b10e04440
Krzysztof Jablonski <jablko@gmail.com>
2010-01-05 18:55:22 +0100

    pojawiający sie search

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -1,2 +1,6 @@
 module ActorsHelper
+  def search_actor?
+    return false if params['search'].blank?
+    !(params['search']['name_like'].blank? and params['search']['movies_title_like'].blank?)
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,6 @@
 module MoviesHelper
+  def search_movie?
+    return false if params['search'].blank?
+    !(params['search']['title_like'].blank? and params['search']['director_like'].blank? and params['search']['actors_name_like'].blank? and params['search']['category_id_equals'].blank?)
+  end
 end
